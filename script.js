0045e51d04096f353d64e9e0b62e30ccbe787eb9
Aman Adlakha <amanady125@gmail.com>
2026-05-01 20:43:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let currentUrl_ = window.location.href;\
+    let currentUrl_ = window.location.href;
     let currentUrl= currentUrl_.substring(0, currentUrl_.lastIndexOf('.'));
     let menuLinks = document.querySelectorAll(".nav-menu a");
     let foundActive = false;
